97a4dbb3532e1abb527a7fd52a04dca987488a20
Simon Vargas <sjvargas01@gmail.com>
2023-07-19 22:16:35 -0400

    created a class

--- lib/pages/more_page.dart
+++ lib/pages/more_page.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+class TileInformation {
+  TileInformation({
+    required this.title,
+    required this.icon,
+  });
+  final String title;
+  final Widget icon;
+}
+
 class MorePage extends StatefulWidget {
   const MorePage({
     super.key,
@@ -17,80 +26,77 @@ class _MorePageState extends State<MorePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: isBlue ? Colors.blueAccent : null,
-      appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-        backgroundColor: Colors.blueAccent,
-        leading: IconButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).clearMaterialBanners();
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-          ),
-        ),
-        actions: [
-          IconButton(
+        backgroundColor: isBlue ? Colors.blueAccent : null,
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+          leading: IconButton(
             onPressed: () {
-              setState(() {
-                numberOfListTile++;
-              });
-            
-          },
-          icon: const Icon(Icons.add),),
-          IconButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).showMaterialBanner(
-                MaterialBanner(
-                  content: const Text('This is a Material Banner'),
-                  actions: <Widget>[
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          isBlue = !isBlue;
-                        });
-                      },
-                      child: const Text('Change color'),
-                    ),
-                    TextButton(
-                      onPressed: () => ScaffoldMessenger.of(context)
-                          .hideCurrentMaterialBanner(),
-                      child: const Text('DISMISS'),
-                    ),
-                    OutlinedButton(
-                      onPressed: () {
-                        ScaffoldMessenger.of(context)
-                            .removeCurrentMaterialBanner();
-                      },
-                      child: const Text('REMOVE'),
-                    ),
-                  ],
-                ),
-              );
+              ScaffoldMessenger.of(context).clearMaterialBanners();
+              Navigator.of(context).pop();
             },
-            icon: const Icon(Icons.more_horiz),
+            icon: const Icon(
+              Icons.arrow_back,
+            ),
           ),
-        ],
-      ),
-      body: ListView.separated(
-        itemBuilder:(context, index) {
-          return const ListTile(
-          title: Text('Item'),
-          leading: Icon(Icons.radio_button_checked),
-          trailing: Icon(Icons.radio_button_checked),
-        );
-        },
-        itemCount: numberOfListTile,
-        separatorBuilder: (context, index){
-          return const Divider(thickness: 2,);
-        },
-
-        ) 
-      
-      
-
-    );
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  numberOfListTile++;
+                });
+              },
+              icon: const Icon(Icons.add),
+            ),
+            IconButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).showMaterialBanner(
+                  MaterialBanner(
+                    content: const Text('This is a Material Banner'),
+                    actions: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            isBlue = !isBlue;
+                          });
+                        },
+                        child: const Text('Change color'),
+                      ),
+                      TextButton(
+                        onPressed: () => ScaffoldMessenger.of(context)
+                            .hideCurrentMaterialBanner(),
+                        child: const Text('DISMISS'),
+                      ),
+                      OutlinedButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context)
+                              .removeCurrentMaterialBanner();
+                        },
+                        child: const Text('REMOVE'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              icon: const Icon(Icons.more_horiz),
+            ),
+          ],
+        ),
+        body: ListView.separated(
+          itemBuilder: (context, index) {
+            return const ListTile(
+              title: Text('Item'),
+              leading: Icon(Icons.radio_button_checked),
+              trailing: Icon(Icons.radio_button_checked),
+            );
+          },
+          itemCount: numberOfListTile,
+          separatorBuilder: (context, index) {
+            return const Divider(
+              thickness: 2,
+            );
+          },
+        ));
   }
 }
