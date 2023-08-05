e8a01cb81382c86b9dde70c11089f747856ab32f
Simon Vargas <sjvargas01@gmail.com>
2023-08-04 23:00:09 -0400

    changed login page from statefull to stateless

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
