6fa570d3b3d4fc63353176c6c5ea9332837cb084
Simon Vargas <sjvargas01@gmail.com>
2023-08-18 15:21:36 -0400

    updates to register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+TextEditingController controllerEmail = TextEditingController();
+TextEditingController controllerPassword = TextEditingController();
+final formKey = GlobalKey<FormState>();
+
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
@@ -10,6 +14,21 @@ class RegisterPage extends StatelessWidget {
         title: const Text('Register Page'),
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
+      ),
+      body: Form(
+        child: Center(
+          child: Column(
+            children: [
+              Image.asset('images/cactus.png'),
+              TextFormField(
+                controller: controllerEmail,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Email',
+                ),
+              )
+            ]),
+        ),
       ),
     );
   }
