ae1869092dd4f771ce6cbddaf9485a769fb3d756
Simon Vargas <sjvargas01@gmail.com>
2023-09-15 20:36:10 -0400

    added homepage

---
+++ lib/widget_tree.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_welcome/pages/home_page.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return const HomePage();
+        }
+      },
+      stream: null,
+    );
+  }
+}
