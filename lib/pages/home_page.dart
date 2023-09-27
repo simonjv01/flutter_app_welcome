7e5b8aeb70f08c2f7aa312cdb697eb4117d2799e
Simon Vargas <sjvargas01@gmail.com>
2023-09-26 23:57:20 -0400

    updated home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,10 +3,19 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  int currentPage = 0;
+  List<Widget> pages = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Email'),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: pages.elementAt(currentPage),
+    
     );
   }
 }
