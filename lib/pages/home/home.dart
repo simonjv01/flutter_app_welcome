db08af90fe69e0fac17da5a1cb1173b00318c0e5
Simon Vargas <sjvargas01@gmail.com>
2023-11-06 20:43:41 -0500

    added sizedBox between fields

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,23 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_welcome/pages/register_page.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController controllerName = TextEditingController();
+  TextEditingController controllerAge = TextEditingController();
+  bool isAgree = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            TextFormField(
-              controller: controllerEmail,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Email',
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                controller: controllerName,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Email',
+                ),
               ),
-            )
-          ],
+              const SizedBox(height: 20.0),
+              TextFormField(
+                controller: controllerAge,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Age',
+                ),
+              ),
+              const SizedBox(height: 20.0),
+            ],
+          ),
         ),
       ),
     );
