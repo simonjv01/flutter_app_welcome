103a25998afeec15e881970a80c8b3d4e49e8e66
Simon Vargas <sjvargas01@gmail.com>
2023-11-06 20:36:21 -0500

    added textFormfield

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_welcome/pages/register_page.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Home'),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controllerEmail,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Email',
+              ),
+            )
+          ],
+        ),
       ),
-   
     );
   }
 }
